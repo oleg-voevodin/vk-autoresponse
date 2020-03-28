cdb427a393c7f802e0e0efd3fc64f8346987b43b
Oleg <oleg-voevodin02@protonmail.com>
2020-03-28 08:10:44 +0300

    Fix

--- autoresponce-cli.py
+++ autoresponce-cli.py
@@ -2,9 +2,6 @@ import vk_api
 import random
 import time
 
-def write_msg(user_id, msg):
-    vk.method('messages.send', {'user_id':user_id, 'message':msg, 'random_id': random.randint(-9999999999999, 9999999999)})
-
 try:
     login = input('Enter VK login: ')
     password = input('Enter VK password: ')
@@ -31,19 +28,36 @@ try:
         if response['items']:
             values['last_message_id'] = response['items'][0]['conversation']['peer']['id']
             if response['items'][0]['conversation']['peer']['id'] not in need_for_answer:
-                need_for_answer.append(response['items'][0]['conversation']['peer']['id'])
+                need_for_answer.append(dict(peer_id=response['items'][0]['conversation']['peer']['id'], peer_type=response['items'][0]['conversation']['peer']['type']))
         for item in response['items']:
-            write_msg(item['conversation']['peer']['id'],message)
+            # VK have 3 types of chats, depends from chat object: 
+            # 'user' - vk user
+            # 'chat' - vk chat (multiusers conversation)
+            # 'group' - vk group (chat with group)
+
+            if item['conversation']['peer']['type'] == 'user': 
+                vk.method('messages.send', {'user_id': item['conversation']['peer']['id'], 'message': message, 'random_id': random.randint(-999999999, 999999999)})
+            elif item['conversation']['peer']['type'] == 'chat':
+                vk.method('messages.send', {'peer_id': item['conversation']['peer']['id'], 'message': message, 'random_id': random.randint(-999999999, 999999999)})
+            elif item['conversation']['peer']['type'] == 'group':
+                vk.method('messages.send', {'peer_id': item['conversation']['peer']['id'], 'message': message, 'random_id': random.randint(-999999999, 999999999)})
         vk.method('account.setOnline')
         time.sleep(3)
 except KeyboardInterrupt:
     print('Stopped.')
     if len(need_for_answer) != 0:
         for k in need_for_answer:
-            first_name = vk.method('users.get', {'user_ids':k})[0]['first_name']
-            last_name = vk.method('users.get', {'user_ids': k})[0]['last_name']
-            print(f'You need answer to {first_name} {last_name}.')
-            write_msg(k, 'Answering machine is turned off!')
+            if k['peer_type'] == 'user':
+                first_name = vk.method('users.get', {'user_ids':k['peer_id']})[0]['first_name']
+                last_name = vk.method('users.get', {'user_ids': k['peer_id']})[0]['last_name']
+                print(f'You need answer to {first_name} {last_name}.')
+            elif k['peer_type'] == 'chat':
+                chat_title = vk.method('messages.getConversationsById', {'peer_ids': k['peer_id']})['items'][0]['chat_settings']['title']
+                print(f'You need answer in \'{chat_title}\' chat.')
+            elif k['peer_type'] == 'group':
+                group_title = vk.method('groups.getById', {'group_id': -k['peer_id']})[0]['name']
+                print(f'You need to answer for \'{group_title}\' group.')
+
     else:
         print('You have no unanswered messages.')
     print('Goodbye :D'); input(); exit()
